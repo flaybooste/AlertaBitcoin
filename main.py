14c5bae9b65c1ee53197798e87fa2651ef876032
Luis Eduardo <retardado.tw@gmail.com>
2017-11-01 14:12:08 -0200

    VENDA MUSICA MBT
    Agora se o valor de venda no mercado bitcoin for maior que 23600 ele toca o ringtone também

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from pygame import mixer
 
 with open("arquivo.csv", "w") as _file:
     _file.write("hora, compra,venda ,moeda, fonte, ID\n")
-    Cont = 1 
-    while True: 
+    Cont = 1
+    while True:
         if(Cont%2 == 1):
             _file.write(str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)+","+str(vBit.Mbt(1))+","+str(vBit.Mbt(2))+","+" Bitcoin"+","+"MBT"+","+str(Cont)+"\n")
+            if(float(vBit.Mbt(2)) > 23600):
+                mixer.init()
+                mixer.music.load("ring.mp3")
+                mixer.music.play()
             print(str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)+","+str(vBit.Mbt(1))+","+str(vBit.Mbt(2))+","+" Bitcoin"+","+"MBT"+","+str(Cont)+"\n")
             Cont += 1
             from time import sleep
