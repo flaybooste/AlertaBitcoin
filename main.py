c870ee091bdf073d1079be339b7ac589812b0dc2
Luis Eduardo <retardado.tw@gmail.com>
2017-11-02 13:48:09 -0200

    Alt Musicas
    Alterações na música

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ with open("arquivo.csv", "w") as _file:
     while True:
         if(Cont%2 == 1):
             _file.write(str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)+","+str(vBit.Mbt(1))+","+str(vBit.Mbt(2))+","+" Bitcoin"+","+"MBT"+","+str(Cont)+"\n")
-            if(float(vBit.Mbt(2)) > 23600):
+            '''if(float(vBit.Mbt(1)) > 24000):
                 mixer.init()
                 mixer.music.load("ring.mp3")
-                mixer.music.play()
+                mixer.music.play()'''
             print(str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)+","+str(vBit.Mbt(1))+","+str(vBit.Mbt(2))+","+" Bitcoin"+","+"MBT"+","+str(Cont)+"\n")
             Cont += 1
             from time import sleep
             sleep(10)
         if(Cont%2 == 0):
             _file.write(str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)+","+str(vBit.Nc(1))+","+str(vBit.Nc(2))+","+" Bitcoin"+","+"NC"+","+str(Cont)+"\n")
-            if(vBit.Nc(2) > 23600):
+            '''if(vBit.Nc(1) > 24000):
+                mixer.init()
+                mixer.music.load("ring.mp3")
+                mixer.music.play()'''
+            if(vBit.Nc(2) > 24800):
                 mixer.init()
                 mixer.music.load("ring.mp3")
                 mixer.music.play()
